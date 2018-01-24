10b113eff0b109252b3f6b0f17223334d28a60d4
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2018-01-24 08:57:13 -0500

    Removed a1q3

--- a1q3/__main__.py
+++
@@ -1,5 +0,0 @@
-import sys
-import a1q3
-
-if __name__ == '__main__':
-    sys.exit (a1q3.main (sys.argv))
--- a1q3/test.py
+++
@@ -1,7 +0,0 @@
-import sys
-import unittest
-
-if __name__ == '__main__':
-    name = 'a1q3.coverage_tests'
-    suite = unittest.defaultTestLoader.loadTestsFromNames ([name])
-    result = unittest.TextTestRunner().run (suite)
