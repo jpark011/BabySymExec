e82be99ff79649e9de4e91eb4a25069f0a6ae841
Jay Park <wngt3319@gmail.com>
2018-03-07 22:46:12 -0500

    Done q3

--- a2q3/puzzle_tests.py
+++ a2q3/puzzle_tests.py
@@ -11,18 +11,28 @@ class PuzzleTests (unittest.TestCase):
         """Reset Z3 context after test"""
         import z3
         z3._main_ctx = None
-        
+
+    def test_0 (self):
+        """BASE CASE"""
+        res = solve ('SEND', '', 'SEND')
+        self.assertEquals (res, (9210, 0, 9210))
+
     def test_1 (self):
         """SEND + MORE = MONEY"""
         res = solve ('SEND', 'MORE', 'MONEY')
         self.assertEquals (res, (9567, 1085, 10652))
 
     def test_2 (self):
-        pass
+        """When s3 is shorter!! invalid"""
+        res = solve ('SEND', 'MORE', 'CAT')
+        self.assertEquals (res, None)
 
     def test_3 (self):
-        pass
-    
+        """Unsat solution"""
+        res = solve ('AAAA', 'BBBB', 'ABCDE')
+        self.assertEquals (res, None)
+
     def test_4 (self):
-        pass
-        
+        """When s3 is too long!"""
+        res = solve ('SEND', 'MORE', 'CMONEY')
+        self.assertEquals (res, None)
--- a2q3/verbal_arithmetic.py
+++ a2q3/verbal_arithmetic.py
@@ -14,14 +14,93 @@ def mk_var (x):
 def get_vars ():
     return vars.values ()
 
+def to_solution (s, model):
+    if len(s) is 0:
+        return 0
+    ret = ""
+
+    for c in s:
+        ret += model[mk_var(c)].as_string()
+
+    return int(ret)
+
 
 def solve (s1, s2, s3):
     global vars
     vars = dict()
 
-    # Replace with your solution
-    pass
+    # z3 setUp
+    solver = z3.Solver()
 
+    # get len
+    s1_len = len(s1)
+    s2_len = len(s2)
+    s3_len = len(s3)
+
+    # s3 should be longest
+    solver.add(z3.IntVal(s1_len) <= z3.IntVal(s3_len))
+    solver.add(z3.IntVal(s2_len) <= z3.IntVal(s3_len))
+
+    # reverse all strings
+    s1_r = s1[::-1]
+    s2_r = s2[::-1]
+    s3_r = s3[::-1]
+
+    # iterate 0 to s3_len
+    for i in range(s3_len):
+        # set c1
+        if i < s1_len:
+            c1 = mk_var(s1_r[i])
+        else:
+            c1 = z3.IntVal(0)
+
+        # set c1_prev
+        if 0 < i and i <= s1_len:
+            c1_prev = mk_var(s1_r[i-1])
+        else:
+            c1_prev = z3.IntVal(0)
+
+
+        # set c2
+        if i < s2_len:
+            c2 = mk_var(s2_r[i])
+        else:
+            c2 = z3.IntVal(0)
+
+        # set c2_prev
+        if 0 < i and i <= s2_len:
+            c2_prev = mk_var(s2_r[i-1])
+        else:
+            c2_prev = z3.IntVal(0)
+
+        #set c3
+        c3 = mk_var(s3_r[i])
+
+        # assertion
+        # first when it's the first ele
+        solver.add(c3 == (c1 + c2 + (c1_prev + c2_prev) / 10) % 10)
+
+    # make sure each var is single digit
+    for v in get_vars():
+        solver.add(v <= 9)
+        solver.add(0 <= v)
+
+    # all solutions should be distinct
+    solver.add(z3.Distinct(get_vars()))
+
+    # solve
+    res = solver.check()
+
+    if res == z3.sat:
+        model = solver.model()
+
+        sol1 = to_solution(s1, model)
+        sol2 = to_solution(s2, model)
+        sol3 = to_solution(s3, model)
+
+        return (sol1, sol2, sol3)
+    else:
+        return None
 
 def print_sum (s1, s2, s3):
     s1 = str(s1)
@@ -32,7 +111,7 @@ def print_sum (s1, s2, s3):
     print s2.rjust (len(s3) + 1)
     print ' ' + ('-'*(len(s3)))
     print s3.rjust (len(s3) + 1)
-    
+
 def puzzle (s1, s2, s3):
     print_sum (s1, s2, s3)
     res = solve (s1, s2, s3)
@@ -41,6 +120,6 @@ def puzzle (s1, s2, s3):
     else:
         print 'Solution:'
         print_sum (res[0], res[1], res[2])
-        
+
 if __name__ == '__main__':
     puzzle ('SEND', 'MORE', 'MONEY')
